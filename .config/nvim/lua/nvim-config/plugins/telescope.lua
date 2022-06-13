ce11ca57f196f1960125d514ff93e04700ba2bd9
Bjarki Baldursson Harksen <bjarki31@gmail.com>
2022-06-13 12:28:59 +0000

    setup telescope

--- .config/nvim/lua/nvim-config/plugins/telescope.lua
+++ .config/nvim/lua/nvim-config/plugins/telescope.lua
@@ -1,14 +1,58 @@
 require'telescope'.setup {
   defaults = {
     mappings = {
-      i = require'nvim-config.keymaps'.telescope.active(),
-      n = require'nvim-config.keymaps'.telescope.active(),
+      n = {
+        ['P'] = require'telescope.actions.layout'.toggle_preview,
+        ['<C-Up>'] = 'cycle_history_prev',
+        ['<C-Down>'] = 'cycle_history_next',
+        ['<C-Left>'] = 'cycle_previewers_next',
+        ['<C-Right>'] = 'cycle_previewers_prev',
+      },
+      i = {
+        ['<C-H>'] = 'which_key',
+        ['<C-Up>'] = 'cycle_history_prev',
+        ['<C-Down>'] = 'cycle_history_next',
+        ['<C-Left>'] = 'cycle_previewers_next',
+        ['<C-Right>'] = 'cycle_previewers_prev',
+      },
     },
-    layout_config = { scroll_speed = 3 },
-    prompt_prefix = ' ',
+    -- prompt_prefix = ' ',
+    prompt_prefix = '  ',
     selection_caret = ' ',
     multi_icon = '┃',
-    --path_display = { 'smart' },
+  },
+  pickers = {
+    builtin = {
+      theme = 'dropdown',
+      preview = { hide_on_startup = true },
+      include_extensions = true,
+      use_default_opts = true,
+    },
+    commands             = { theme = 'ivy'      },
+    quickfix             = { theme = 'ivy'      },
+    quickfixhistory      = { theme = 'ivy'      },
+    loclist              = { theme = 'ivy'      },
+    command_history      = { theme = 'ivy'      },
+    search_history       = { theme = 'ivy'      },
+    help_tags            = { theme = 'ivy'      },
+    man_pages            = { theme = 'ivy'      },
+    colorscheme          = { theme = 'dropdown' },
+    keymaps              = { theme = 'dropdown' },
+    filetypes            = { theme = 'dropdown' },
+    highlights           = { theme = 'dropdown' },
+    autocommands         = { theme = 'dropdown' },
+    spell_suggest        = { theme = 'cursor'   },
+    lsp_references       = { theme = 'cursor'   },
+    lsp_definitions      = { theme = 'cursor'   },
+    lsp_type_definitions = { theme = 'cursor'   },
+    lsp_implementations  = { theme = 'cursor'   },
+    diagnostics          = { theme = 'ivy'      },
+  },
+  extensions = {
+    packer = {
+      theme = 'dropdown',
+      preview = { hide_on_startup = true },
+    },
   }
 }
 
