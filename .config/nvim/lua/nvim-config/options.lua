28da73ef3af216d125c1fe09f34fd4e28582e58e
Bjarki Baldursson Harksen <bjarki31@gmail.com>
2022-06-13 12:27:07 +0000

    change options

--- .config/nvim/lua/nvim-config/options.lua
+++ .config/nvim/lua/nvim-config/options.lua
@@ -1,11 +1,12 @@
 -- QoL
-vim.opt.mouse       = 'a'
-vim.opt.number      = true
-vim.opt.splitbelow  = true
-vim.opt.splitright  = true
-vim.opt.whichwrap   = { ['b'] = true, ['<'] = true , ['>'] = true, ['['] = true, [']'] = true }
-vim.opt.showmode    = false
-vim.opt.lazyredraw  = true
+vim.opt.mouse      = 'a'
+vim.opt.number     = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.whichwrap  = { ['b'] = true, ['<'] = true , ['>'] = true, ['['] = true, [']'] = true }
+vim.opt.showmode   = false
+vim.opt.lazyredraw = true
+vim.opt.ruler      = false
 
 -- Scrolloff
 vim.opt.scrolloff     = 3
@@ -40,4 +41,5 @@ vim.opt.pumheight = 5
 vim.opt.updatetime = 500
 
 -- Display signs in number column
-vim.opt.signcolumn = 'number'
+vim.opt.signcolumn = 'yes:1'
+-- vim.opt.signcolumn = 'number'
