ab91d371bc229b5177de9c42f460faa0d3793a26
Bjarki Baldursson Harksen <bjarki31@gmail.com>
2022-08-22 23:52:20 +0000

    xmonad workspaces in smobar

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -4,12 +4,16 @@ import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 
 statusbar :: StatusBarConfig
-statusbar = statusBarProp "xmobar ~/.config/xmobar/xmobarrc" (pure def)
+statusbar = statusBarProp "xmobar ~/.config/xmobar/xmobarrc" $ pure def
+  { ppCurrent = const "\xf111"
+  , ppHidden = const "\xf10c"
+  , ppOrder = \(ws:_) -> [ws]
+  }
 
 settings = def
-    { terminal = "kitty"
-    , borderWidth = 1
-    }
+  { terminal = "kitty"
+  , borderWidth = 1
+  }
 
 main :: IO ()
 main = xmonad . ewmhFullscreen . ewmh . withSB statusbar $ settings 
