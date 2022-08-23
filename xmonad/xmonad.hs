988314c938416fc9ed2b51ad153d3e7ffb369e45
Bjarki Baldursson Harksen <bjarki31@gmail.com>
2022-08-23 14:37:35 +0000

    set default cursor

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,6 +2,8 @@ import XMonad
 import XMonad.Hooks.EwmhDesktops (ewmhFullscreen, ewmh)
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
+import XMonad.Util.Cursor (setDefaultCursor)
+import Graphics.X11 (xC_left_ptr)
 
 statusbar :: StatusBarConfig
 statusbar = statusBarProp "xmobar ~/.config/xmobar/xmobarrc" $ pure def
@@ -11,10 +13,10 @@ statusbar = statusBarProp "xmobar ~/.config/xmobar/xmobarrc" $ pure def
   , ppOrder = take 1
   }
 
-settings = def
+main :: IO ()
+main = xmonad . ewmhFullscreen . ewmh . withEasySB statusbar defToggleStrutsKey $ def
   { terminal = "kitty"
   , borderWidth = 1
+  , startupHook = setDefaultCursor xC_left_ptr
   }
 
-main :: IO ()
-main = xmonad . ewmhFullscreen . ewmh . withEasySB statusbar defToggleStrutsKey $ settings 
