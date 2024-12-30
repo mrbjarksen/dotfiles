6079e7aec878bdc3a8ca58b8dce788cc48a6787e
Bjarki B. Harksen <bjarki@harksen.is>
2024-12-30 01:56:02 +0000

    locale fix

--- home/modules/common.nix
+++ home/modules/common.nix
@@ -15,21 +15,21 @@
     fzf
   ];
 
-  home.language = {
-    base = "en_US.UTF-8";
-
-    time        = "en_DK.UTF-8";
-    collate     = "is_IS.UTF-8";
-    monetary    = "is_IS.UTF-8";
-    paper       = "is_IS.UTF-8";
-    telephone   = "is_IS-UTF-8";
-    measurement = "is_IS.UTF-8";
-
-    ctype    = "en_US.UTF-8";
-    messages = "en_US.UTF-8";
-    address  = "en_US.UTF-8";
-    name     = "en_US.UTF-8";
-  };
+  # home.language = {
+  #   base = "en_US.UTF-8";
+  #
+  #   time        = "en_DK.UTF-8";
+  #   collate     = "is_IS.UTF-8";
+  #   monetary    = "is_IS.UTF-8";
+  #   paper       = "is_IS.UTF-8";
+  #   telephone   = "is_IS-UTF-8";
+  #   measurement = "is_IS.UTF-8";
+  #
+  #   ctype    = "en_US.UTF-8";
+  #   messages = "en_US.UTF-8";
+  #   address  = "en_US.UTF-8";
+  #   name     = "en_US.UTF-8";
+  # };
 
   programs.firefox.enable = true; # <-
   programs.imv.enable = true; # <-
--- system/modules/region.nix
+++ system/modules/region.nix
@@ -2,7 +2,16 @@
 
 {
   i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings.LC_TIME = "is_IS.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LANG = "en_US.UTF-8";
+    LC_TIME = "en_DK.UTF-8";
+    LC_COLLATE = "is_IS.UTF-8";
+    LC_MONETARY = "is_IS.UTF-8";
+    LC_PAPER = "is_IS.UTF-8";
+    LC_TELEPHONE = "is_IS-UTF-8";
+    LC_MEASUREMENT = "is_IS.UTF-8";
+  };
 
   services.automatic-timezoned.enable = true;
   services.geoclue2.enable = true;
