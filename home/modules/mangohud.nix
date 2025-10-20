18ff7b5cf74ba163f460c46f3725e36b11a03fce
Bjarki B. Harksen <bjarki@harksen.is>
2025-10-20 16:30:28 +0000

    re-enable mangohud config

--- home/modules/mangohud.nix
+++ home/modules/mangohud.nix
@@ -3,34 +3,35 @@
 {
   programs.mangohud = {
     enable = true;
-    settings.fps_only = true;
-    # settings = {
-    #   fps = true;
-    #   frametime = false;
-    #   frame_timing = false;
-    #
-    #   cpu_stats = true;
-    #   ram = true;
-    #
-    #   gpu_stats = true;
-    #   vram = true;
-    #
-    #   font_file =  "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/NerdFonts/JetBrainsMono/JetBrainsMono-Light.ttf";
-    #   font_size = 13;
-    #   hud_compact = true;
-    #   hud_no_margin = true;
-    #   offset_x = 3;
-    #   horizontal = true;
-    #
-    #   cpu_color = "5A5A5A";
-    #   gpu_color = "5A5A5A";
-    #   ram_color = "5A5A5A";
-    #   vram_color = "5A5A5A";
-    #   text_color = "A0A0A0";
-    #   text_outline_thickness = 0.1;
-    #   background_alpha = 0;
-    #
-    #   toggle_hud_preset = "Super_L+Alt_L+M";
-    # };
+    enableSessionWide = true;
+    settings = {
+      fps = true;
+      fps_limit = 120;
+      frametime = false;
+      frame_timing = false;
+
+      cpu_stats = true;
+      ram = true;
+
+      gpu_stats = true;
+      vram = true;
+
+      font_file = "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/NerdFonts/JetBrainsMono/JetBrainsMono-Light.ttf";
+      font_size = 13;
+      hud_compact = true;
+      hud_no_margin = true;
+      offset_x = 3;
+      horizontal = true;
+
+      cpu_color = "5A5A5A";
+      gpu_color = "5A5A5A";
+      ram_color = "5A5A5A";
+      vram_color = "5A5A5A";
+      text_color = "A0A0A0";
+      text_outline_thickness = 0.1;
+      background_alpha = 0;
+
+      toggle_hud_preset = "Super_L+Alt_L+M";
+    };
   };
 }
