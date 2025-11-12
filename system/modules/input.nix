848ad32d56a2bc30739d8150e9646485825ad3da
Bjarki B. Harksen <bjarki@harksen.is>
2025-11-12 00:53:56 +0000

    input: add udev rule for 8BitDo Ultimate 2

--- system/modules/input.nix
+++ system/modules/input.nix
@@ -8,4 +8,7 @@
     # lcd.enable = true;
   };
   services.udev.packages = [ pkgs.dolphin-emu ];
+  services.udev.extraRules = ''
+    SUBSYSTEM=="INPUT", ATTRS{idVendor}=="2dc8", ATTRS{idProduct}=="310b", MODE="0660" GROUP="INPUT"
+  '';
 }
