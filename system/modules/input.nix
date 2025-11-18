1fa0a0481637dfe9a3d2b99629dd38fabc622252
Bjarki B. Harksen <bjarki@harksen.is>
2025-11-18 20:20:29 +0000

    input: fix udev rule

--- system/modules/input.nix
+++ system/modules/input.nix
@@ -7,8 +7,15 @@
     wireless.enableGraphical = true;
     # lcd.enable = true;
   };
-  services.udev.packages = [ pkgs.dolphin-emu ];
-  services.udev.extraRules = ''
-    SUBSYSTEM=="INPUT", ATTRS{idVendor}=="2dc8", ATTRS{idProduct}=="310b", MODE="0660" GROUP="INPUT"
-  '';
+  services.udev.packages = [
+    pkgs.dolphin-emu
+    (pkgs.writeTextFile {
+      name = "8bitdo-u2w-udev-rules";
+      text = ''
+        KERNEL=="hidraw*", ATTRS{idVendor}=="2dc8", MODE="0660", TAG+="uaccess"
+        KERNEL=="hidraw*", KERNELS=="*2DC8:*", MODE="0660", TAG+="uaccess"
+      '';
+      destination = "/etc/udev/rules.d/71-8bitdo-u2w.rules";
+    })
+  ];
 }
